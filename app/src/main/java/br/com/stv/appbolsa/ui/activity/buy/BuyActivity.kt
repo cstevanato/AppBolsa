51c92449bdbd7de0cf6367d42e3d0cd42c96ea63
Claudio Stevanato <cstevanato@gmail.com>
2018-05-24 16:02:50 -0300

    Atualizando tela

--- app/src/main/java/br/com/stv/appbolsa/ui/activity/buy/BuyActivity.kt
+++ app/src/main/java/br/com/stv/appbolsa/ui/activity/buy/BuyActivity.kt
@@ -2,8 +2,11 @@ package br.com.stv.appbolsa.ui.activity.buy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import br.com.stv.appbolsa.R
 import br.com.stv.appbolsa.dao.ISummaryStock
+import kotlinx.android.synthetic.main.activity_buy.*
 
 class BuyActivity : AppCompatActivity(), BuyContract.View {
     override fun showTasks(summaryList: List<ISummaryStock>) {
@@ -17,5 +20,14 @@ class BuyActivity : AppCompatActivity(), BuyContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy)
+
+
+        btn_confirm.setOnClickListener({
+            Toast.makeText(this, "You clicked me.", Toast.LENGTH_SHORT).show()
+
+        })
     }
+
+
+
 }
