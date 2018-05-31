3f00c5e830a972b64da3497d9bf54c33c5d823f1
Claudio Stevanato <cstevanato@gmail.com>
2018-05-31 18:51:06 -0300

    Salvado parte do programa

--- app/src/main/java/br/com/stv/appbolsa/ui/activity/buy/BuyContract.kt
+++ app/src/main/java/br/com/stv/appbolsa/ui/activity/buy/BuyContract.kt
@@ -1,5 +1,6 @@
 package br.com.stv.appbolsa.ui.activity.buy
 
+import br.com.stv.appbolsa.dao.IBuySellStock
 import br.com.stv.appbolsa.dao.ISummaryStock
 import br.com.stv.appbolsa.ui.activity.BasePresenter
 import br.com.stv.appbolsa.ui.activity.BaseView
@@ -9,6 +10,7 @@ interface BuyContract {
     interface Presenter : BasePresenter {
         fun loadSummaries()
         fun syncTask()
+        fun add(buy : IBuySellStock)
     }
 
     interface View: BaseView<Presenter> {
--- app/src/main/java/br/com/stv/appbolsa/ui/activity/buy/BuyPresenter.kt
+++ app/src/main/java/br/com/stv/appbolsa/ui/activity/buy/BuyPresenter.kt
@@ -1,10 +1,20 @@
 package br.com.stv.appbolsa.ui.activity.buy
 
 import android.content.Context
-import br.com.stv.appbolsa.ui.activity.main.SummaryContract
+import br.com.stv.appbolsa.dao.BuyStockDao
+import br.com.stv.appbolsa.dao.IBuySellStock
 
 class BuyPresenter (private val context: Context,
                         private val summaryView: BuyContract.View) : BuyContract.Presenter {
+    override fun add(buy: IBuySellStock) {
+        try {
+            BuyStockDao().insert(buy)
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
     override fun loadSummaries() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -20,4 +30,6 @@ class BuyPresenter (private val context: Context,
     override fun dispose() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+
 }
